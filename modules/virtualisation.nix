98d42df95e5cf50f7c70a8cde461b4dce53caaa6
Lucas Portela <contato.koish@gmail.com>
2023-06-11 20:34:29 -0300

    added: support for virtualisation and containers

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -1,14 +1,38 @@
 {pkgs, ...}: {
-  virtualisation = {
-    libvirtd = {
-      enable = true;
-      qemu = {
-        swtpm.enable = true;
-        ovmf.enable = true;
-        ovmf.packages = [pkgs.OVMFFull.fd];
-      };
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      swtpm.enable = true;
+      ovmf.enable = true;
+      ovmf.packages = [pkgs.OVMFFull.fd];
     };
-    spiceUSBRedirection.enable = true;
   };
+
+  virtualisation.spiceUSBRedirection.enable = true;
   services.spice-vdagentd.enable = true;
+
+  virtualisation.docker = {
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
+    storageDriver = "btrfs";
+  };
+
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+
+    defaultNetwork.settings.dns_enabled = true;
+  };
+
+  virtualisation.oci-containers.backend = "podman";
+  virtualisation.oci-containers.containers = {
+    archive-team-warrior = {
+      image = "atdr.meo.ws/archiveteam/reddit-grab";
+      autoStart = true;
+      cmd = ["nicekoishi"];
+      extraOptions = ["--network=host"];
+    };
+  };
 }
