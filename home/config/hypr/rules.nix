bce3fb214e5a16e8c0847f0934559c1dff959115
Lucas Portela <contato.koish@gmail.com>
2024-03-22 18:18:14 -0300

    feat: nixify hyprland config

---
+++ home/config/hypr/rules.nix
@@ -0,0 +1,78 @@
+{
+  wayland.windowManager.hyprland.settings = {
+    layerrule = [
+      "blur, ^(gtk-layer-shell|wofi|anyrun)$"
+      "ignorezero, ^(gtk-layer-shell|wofi|anyrun)$"
+    ];
+
+    windowrule = [
+      # make apps floaty
+      "float, anyrun"
+      "float, confirm"
+      "float, confirmreset"
+      "float, dialog"
+      "float, download"
+      "float, error"
+      "float, file_progress"
+      "float, lxqt-archiver"
+      "float, notification"
+      "float, pcmanfm-qt"
+      "float, splash"
+      "float, title:DevTools"
+      "float, title:Open File"
+      "float, title:Save File"
+      "float, title:^(Media viewer)$"
+      "float, title:^(Picture-in-Picture)$"
+      "float, title:^(Volume Control)$"
+      "float, title:branchdialog"
+      "float, title:wlogout"
+      "float, Viewnior"
+      "float, viewnior"
+      "float,^(pcmanfm-qt)$"
+
+      # idle inhibit
+      "idleinhibit focus, mpv"
+      "idleinhibit fullscreen, brave"
+
+      # pcmanfm-qt opacity
+      "opacity 0.92, pcmanfm-qt"
+
+      # how wine can be drunk
+      "center, winecfg.exe"
+      "center, zenity"
+      "center,steam_proton"
+      "float, zenity"
+      "nomaxsize, explorer.exe"
+      "nomaxsize, steam_proton"
+      "nomaxsize, winecfg.exe"
+      "size 415 470, steam_proton"
+      "size 415 470, winecfg.exe"
+
+      # didn't they put an anticheat a while ago?
+      "center,^(league of legends.exe)$"
+      "center,^(leagueclientux.exe)$"
+      "forceinput,^(league of legends.exe)$"
+      "nomaxsize, riotclientux.exe"
+      "size 1280 720,^(leagueclientux.exe)$"
+      "size 1280 720,^(riotclientux.exe)$"
+    ];
+
+    windowrulev2 = [
+      # fix xwayland apps
+      "center, class:^(.*jetbrains.*)$, title:^(Confirm Exit|Open Project|win424|win201|splash)$"
+      "rounding 0, xwayland:1, floating:1"
+
+      # oh wait, well too lazy to delete this now
+      "float,workspace 5 silent,class:^(leagueclientux.exe)$,title:^(League of Legends)$"
+      "fullscreen, workspace 5 silent,class:^(league of legends.exe)$,title:^(League of Legends (TM) Client)$ windowrule = size 1920 1080,^(league of legends.exe)$"
+
+      # start apps in n workspace
+      "tile, title:^(Spotify)$"
+      "workspace 3 silent, class:^(WebCord)$"
+      "workspace 4 silent, title:^(Spotify)$"
+      "workspace 5 silent, class:^(lutris*)$"
+      "workspace 5 silent, class:^(steam*)$"
+      "workspace 6 silent, class:^(virt-manager)$"
+    ];
+  };
+}
