4aecfc955535d89d4b07587c0f7e47226f4b9a2f
Lucas Portela <contato.koish@gmail.com>
2024-03-23 02:12:59 -0300

    borrowed a cleaner mimeapps solution

--- home/xdg.nix
+++ home/xdg.nix
@@ -1,37 +1,48 @@
+# okay, this entire file is from fufexan/dotfiles
 {config, ...}: let
-  browser = ["firefox.desktop"];
-  image = ["viewnior.desktop"];
-  video = ["mpv.desktop"];
+  browser = ["brave-browser"];
+  imageViewer = ["viewnior"];
+  videoPlayer = ["mpv"];
 
-  mimeapps = {
-    "application/x-extension-htm" = browser;
-    "application/x-extension-html" = browser;
-    "application/x-extension-shtml" = browser;
-    "application/x-extension-xht" = browser;
-    "application/x-extension-xhtml" = browser;
-    "application/xhtml+xml" = browser;
-    "text/html" = browser;
-    "x-scheme-handler/about" = browser;
-    "x-scheme-handler/chrome" = browser;
-    "x-scheme-handler/ftp" = browser;
-    "x-scheme-handler/http" = browser;
-    "x-scheme-handler/https" = browser;
-    "x-scheme-handler/unknown" = browser;
+  xdgAssociations = type: program: list:
+    builtins.listToAttrs (map (e: {
+        name = "${type}/${e}";
+        value = program;
+      })
+      list);
 
-    "application/json" = browser;
-    "application/pdf" = ["org.pwmt.zathura.desktop.desktop"];
-    "audio/m4a" = video;
-    "audio/mp3" = video;
-    "audio/wav" = video;
-    "image/jpeg" = image;
-    "image/png" = image;
-    "image/webp" = image;
-    "text/plain" = ["emacsclient.desktop"];
-    "video/H264" = video;
-    "video/H265" = video;
-    "x-scheme-handler/discord" = ["webcord.desktop"];
-    "x-scheme-handler/spotify" = browser;
-  };
+  image = xdgAssociations "image" imageViewer ["png" "svg" "jpeg" "gif" "webp"];
+  video = xdgAssociations "video" videoPlayer ["mp4" "avi" "mkv"];
+  browserTypes =
+    (xdgAssociations "application" browser [
+      "json"
+      "x-extension-htm"
+      "x-extension-html"
+      "x-extension-shtml"
+      "x-extension-xht"
+      "x-extension-xhtml"
+    ])
+    // (xdgAssociations "x-scheme-handler" browser [
+      "about"
+      "chrome"
+      "ftp"
+      "http"
+      "https"
+      "unknown"
+    ]);
+
+  # XDG MIME types
+  mimeapps = builtins.mapAttrs (_: v: (map (e: "${e}.desktop") v)) ({
+      "application/pdf" = ["org.pwmt.zathura.desktop"];
+      "text/html" = browser;
+      "text/plain" = ["nvim"];
+      "inode/directory" = ["pcmanfm-qt"];
+      "x-scheme-handler/discord" = ["webcord"];
+      "x-scheme-handler/spotify" = browser;
+    }
+    // image
+    // video
+    // browserTypes);
 in {
   xdg = {
     enable = true;
@@ -46,11 +57,17 @@ in {
       enable = true;
       createDirectories = true;
 
+      publicShare = null;
+      templates = null;
       documents = config.home.homeDirectory + "/Documents";
       download = config.home.homeDirectory + "/Downloads";
       music = config.home.homeDirectory + "/Music";
       pictures = config.home.homeDirectory + "/Pictures";
       videos = config.home.homeDirectory + "/Videos";
+
+      extraConfig = {
+        XDG_SCREENSHOT_DIR = "${config.xdg.userDirs.pictures}/Screenshots";
+      };
     };
   };
 }
