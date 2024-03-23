8c25029007389b10901f6c63e155a69161d3bb77
Lucas Portela <contato.koish@gmail.com>
2024-03-23 01:03:25 -0300

    reboot to windows desktop entry

--- modules/convenience.nix
+++ modules/convenience.nix
@@ -1,5 +1,13 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  windoes = pkgs.makeDesktopItem {
+    desktopName = "Windoes";
+    name = "reboot-to-windows";
+    icon = "distributor-logo-windows";
+    exec = "systemctl reboot --boot-loader-entry=auto-windows";
+  };
+in {
   # TODO: Delete this cursed creation and spread the holy wallpapers
-  environment.systemPackages = with pkgs; [
+  environment.systemPackages = [
+    windoes
   ];
 }
