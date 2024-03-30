594eff6568e4f2ec700ca76cc8dcb199d871d6fa
Lucas Portela <contato.koish@gmail.com>
2024-03-29 21:48:47 -0300

    networking: networkd instead of dhcp script
    saw it on raf's flake, found it cool and copied it
    it may be confirmation bias, but pages seems to load
    faster.
    also, mandatory i may be stupid

--- hosts/polaris/default.nix
+++ hosts/polaris/default.nix
@@ -14,6 +14,28 @@
 
   environment.systemPackages = [pkgs.scx];
 
+  systemd.network = {
+    enable = true;
+
+    wait-online = {
+      enable = false;
+      anyInterface = true;
+    };
+
+    networks = {
+      "10-network-eth" = {
+        matchConfig.type = "en*";
+        networkConfig = {
+          DHCP = "yes";
+          IPv6AcceptRA = true;
+          IPForward = "yes";
+          IPMasquerade = "no";
+        };
+      };
+      # i do have an wifi card, but don't wanna use it. yet
+    };
+  };
+
   services = {
     fstrim.enable = true;
     btrfs.autoScrub.enable = true;
