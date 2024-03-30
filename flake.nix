b7f91e44575e5afa3a06b6d15d5c051e1587c0bf
Lucas Portela <contato.koish@gmail.com>
2024-03-30 10:47:34 -0300

    quickshell: init

--- flake.nix
+++ flake.nix
@@ -28,9 +28,23 @@
     };
 
   inputs = {
-    # nvidia 550 breaks stuff, go back to 535
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    hyprland.url = "github:hyprwm/hyprland";
+    aagl = {
+      url = "github:ezKEa/aagl-gtk-on-nix";
+    };
+
+    ags = {
+      url = "github:Aylur/ags";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    anyrun.url = "github:Kirottu/anyrun";
+
+    arrpc = {
+      url = "github:notashelf/arrpc-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
 
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
@@ -42,30 +56,14 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    ags = {
-      url = "github:Aylur/ags";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    aagl = {
-      url = "github:ezKEa/aagl-gtk-on-nix";
-    };
-
-    arrpc = {
-      url = "github:notashelf/arrpc-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    anyrun.url = "github:Kirottu/anyrun";
-
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
-
     hypr-contrib = {
       url = "github:hyprwm/contrib";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     hypridle.url = "github:hyprwm/hypridle";
+
+    hyprland.url = "github:hyprwm/hyprland";
 
     neovim-flake = {
       url = "github:NotAShelf/neovim-flake";
@@ -76,7 +74,15 @@
 
     nix-gaming.url = "github:fufexan/nix-gaming";
 
+    # nvidia 550 breaks stuff, go back to 535
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     nur.url = "github:nix-community/nur";
+
+    quickshell = {
+      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     xdph = {
       url = "github:hyprwm/xdg-desktop-portal-hyprland";
