fb1dbbef3388eaf487629cec7369ddb56101604e
Lucas Portela <contato.koish@gmail.com>
2024-03-29 21:48:47 -0300

    networking: networkd instead of dhcp script
    saw it on raf's flake, found it cool and copied it
    it may be confirmation bias, but pages seems to load
    faster.
    also, mandatory i may be stupid

--- modules/services/system/network.nix
+++ modules/services/system/network.nix
@@ -1,14 +1,46 @@
-{
+{lib, ...}: let
+  inherit (lib) mkDefault mkForce;
+in {
   networking = {
-    nameservers = ["2606:4700:4700::1111 2606:4700:4700::1001"];
+    useDHCP = mkForce false;
+    useNetworkd = mkForce true;
+    usePredictableInterfaceNames = mkDefault true;
+
+    nameservers = [
+      "1.1.1.1"
+      "1.0.0.1"
+      "2606:4700:4700::1111"
+      "2606:4700:4700::1001"
+    ];
+
+    networkmanager = {
+      enable = true;
+      plugins = mkForce [];
+      dns = "systemd-resolved";
+      unmanaged = [
+        "interface-name:virbr*"
+      ];
+    };
+
     stevenblack = {
       enable = true;
       block = ["gambling" "porn"];
     };
   };
 
-  services.openssh = {
-    enable = true;
-    settings.PasswordAuthentication = false;
+  services = {
+    resolved = {
+      enable = true;
+      dnssec = "false";
+      dnsovertls = "true";
+      fallbackDns = ["9.9.9.9"];
+    };
+
+    openssh = {
+      enable = true;
+      settings.PasswordAuthentication = false;
+    };
   };
+
+  systemd.services.NetworkManager-wait-online.enable = mkForce false;
 }
