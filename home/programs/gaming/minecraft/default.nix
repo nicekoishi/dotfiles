112b176c08247dd1f573c907c13a2f10b5929beb
Lucas Portela <contato.koish@gmail.com>
2024-04-05 20:21:13 -0300

    prism-launcher: catppuccin macchiato >> qt system
    i'm already using mocha on qt, and prism-launcher respects it

--- home/programs/gaming/minecraft/default.nix
+++ home/programs/gaming/minecraft/default.nix
@@ -1,9 +1,4 @@
 {pkgs, ...}: let
-  macchiato = pkgs.fetchzip {
-    url = "https://github.com/catppuccin/prismlauncher/raw/main/themes/Macchiato/Catppuccin-Macchiato.zip";
-    sha256 = "1m6nh7jzfkklhyazvh8navy8wkh2ghcm6xy1002dnrsmk6wgkdky";
-  };
-
   javaPackages = with pkgs; [
     # Java 8
     temurin-jre-bin-8
@@ -23,18 +18,11 @@
     zulu
   ];
 in {
-  home = {
-    file.".local/share/PrismLauncher/themes/macchiato" = {
-      source = macchiato;
-      recursive = true;
-    };
-
-    packages = [
-      (pkgs.prismlauncher.override {
-        # this entire file was copy pasted from raf
-        # thanks <3
-        jdks = javaPackages;
-      })
-    ];
-  };
+  home.packages = [
+    (pkgs.prismlauncher.override {
+      # this entire file was copy pasted from raf
+      # thanks <3
+      jdks = javaPackages;
+    })
+  ];
 }
