e66d04e8743421689febbe840269cdd780d46e22
Lucas Portela <contato.koish@gmail.com>
2024-04-04 21:23:38 -0300

    nix: changing settings

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -1,18 +1,74 @@
-{pkgs, ...}: {
+{
+  config,
+  inputs,
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [./substituters.nix];
-  nix.settings = {
-    experimental-features = ["nix-command" "flakes"];
-    builders-use-substitutes = true;
-    auto-optimise-store = true;
-    keep-going = true;
-    warn-dirty = false;
-    max-jobs = "auto";
-    http-connections = 0;
+  nix = {
+    # pin the registry to avoid downloading and evaluating a new nixpkgs version every time
+    registry = lib.mapAttrs (_: v: {flake = v;}) inputs;
 
-    trusted-users = [
-      "root"
-      "@wheel"
-    ];
+    # set the path for channels compat
+    nixPath = lib.mapAttrsToList (key: _: "${key}=flake:${key}") config.nix.registry;
+
+    # i was doing it manually before. am i stupid?
+    gc = {
+      automatic = true;
+      dates = "Mon *-*-* 03:00";
+      options = "--delete-older-than 7d";
+    };
+
+    # if auto-optimise-store is on, why this?
+    optimise = {
+      automatic = true;
+      dates = ["04:00"];
+    };
+
+    settings = {
+      extra-experimental-features = [
+        "nix-command"
+        "flakes"
+        "cgroups"
+      ];
+
+      flake-registry = "/etc/nix/registry.json";
+
+      # crappy cpu + compiling = a bad time
+      # one day I will have another build machine
+      builders-use-substitutes = true;
+
+      # automatically optimise symlinks, saving space
+      auto-optimise-store = true;
+
+      keep-going = true;
+
+      # i know, shut up
+      warn-dirty = false;
+
+      max-jobs = "auto";
+
+      use-cgroups = true;
+
+      # why limit it? oh wait
+      http-connections = 0;
+
+      # log lines for failed builds
+      log-lines = 50;
+
+      trusted-users = ["root" "@wheel"];
+    };
+  };
+
+  documentation = {
+    doc.enable = false;
+    nixos.enable = true;
+    info.enable = false;
+    man = {
+      enable = lib.modules.mkDefault true;
+      generateCaches = lib.modules.mkDefault true;
+    };
   };
 
   nixpkgs.config.allowUnfree = true;
