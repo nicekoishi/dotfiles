191f38bc466b99fe0c4cba812454a08c62fe8261
Lucas Portela <contato.koish@gmail.com>
2024-04-04 23:12:36 -0300

    nh: zsh function to ask by default

--- home/terminal/shell/zsh.nix
+++ home/terminal/shell/zsh.nix
@@ -24,6 +24,14 @@
     };
 
     initExtra = ''
+      # C-right / C-left for word skips
+      bindkey "^[[1;5C" forward-word
+      bindkey "^[[1;5D" backward-word
+
+      # C-Backspace / C-Delete for word deletions
+      bindkey "^[[3;5~" kill-word
+      bindkey "^H" backward-kill-word
+
       # case insensitive tab completion
       zstyle ':completion:*' completer _complete _ignored _approximate
       zstyle ':completion:*' list-colors '\'
@@ -32,7 +40,16 @@
       zstyle ':completion:*' menu select
       zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
       zstyle ':completion:*' verbose true
+
+      # use cache for completions
+      zstyle ':completion:*' use-cache on
+      zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
       _comp_options+=(globdots)
+
+      # shouldn't it ask by default?
+      nh() {
+        command nh "$@" --ask
+      }
 
       ${lib.optionalString config.services.gpg-agent.enable ''
         gnupg_path=$(ls $XDG_RUNTIME_DIR/gnupg)
