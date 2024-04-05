3a4668b1aeff5e9dfb066d36222b0ea5bc5c0dbb
Lucas Portela <contato.koish@gmail.com>
2024-04-05 14:53:22 -0300

    gamemode: adding stuff
    added notification to gamemode start and end
    obscure kernel param i used before and forgot to add it again

--- modules/programs/gaming/gamemode.nix
+++ modules/programs/gaming/gamemode.nix
@@ -1,25 +1,30 @@
 # straight from fufexan/dotfiles, go check it out
 {
-  config,
+  inputs,
   pkgs,
   lib,
   ...
 }: let
-  programs = lib.makeBinPath [
-    config.programs.hyprland.package
-    pkgs.coreutils
-  ];
+  programs = lib.makeBinPath (with pkgs; [
+    inputs.hyprland.packages."${pkgs.system}".default
+    coreutils
+    libnotify
+  ]);
 
   startscript = pkgs.writeShellScript "gamemode-start" ''
     export PATH=$PATH:${programs}
     export HYPRLAND_INSTANCE_SIGNATURE=$(ls -1 /tmp/hypr | tail -1)
     hyprctl --batch 'keyword decoration:blur:enabled 0 ; keyword animations:enabled 0'
+
+    notify-send -i 'computer-symbolic' 'Gamemode started!' 'Disabling useless stuff' -u 'low'
   '';
 
   endscript = pkgs.writeShellScript "gamemode-end" ''
     export PATH=$PATH:${programs}
     export HYPRLAND_INSTANCE_SIGNATURE=$(ls -1 /tmp/hypr | tail -1)
     hyprctl --batch 'keyword decoration:blur:enabled 1 ; keyword animations:enabled 1'
+
+    notify-send -i 'computer-symbolic' 'Gamemode ended!' 'Enabling the pretty stuff' -u 'low'
   '';
 in {
   programs.gamemode = {
@@ -34,5 +39,11 @@ in {
         end = endscript.outPath;
       };
     };
+  };
+
+  boot.kernel.sysctl = {
+    # this is default on Fedora, and I used it on Arch too
+    # TODO: remember all the stuff I forgot to add
+    "vm.max_map_count" = 2147483642;
   };
 }
