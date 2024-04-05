1df9938bcf7a90df17566495bfdc7ef6a33314e0
Lucas Portela <contato.koish@gmail.com>
2024-04-05 14:37:22 -0300

    gamemode: handling deprecated item

--- modules/programs/gaming/gamemode.nix
+++ modules/programs/gaming/gamemode.nix
@@ -13,13 +13,13 @@
   startscript = pkgs.writeShellScript "gamemode-start" ''
     export PATH=$PATH:${programs}
     export HYPRLAND_INSTANCE_SIGNATURE=$(ls -1 /tmp/hypr | tail -1)
-    hyprctl --batch 'keyword decoration:blur 0 ; keyword animations:enabled 0 ; keyword misc:vfr 0'
+    hyprctl --batch 'keyword decoration:blur:enabled 0 ; keyword animations:enabled 0'
   '';
 
   endscript = pkgs.writeShellScript "gamemode-end" ''
     export PATH=$PATH:${programs}
     export HYPRLAND_INSTANCE_SIGNATURE=$(ls -1 /tmp/hypr | tail -1)
-    hyprctl --batch 'keyword decoration:blur 1 ; keyword animations:enabled 1 ; keyword misc:vfr 1'
+    hyprctl --batch 'keyword decoration:blur:enabled 1 ; keyword animations:enabled 1'
   '';
 in {
   programs.gamemode = {
