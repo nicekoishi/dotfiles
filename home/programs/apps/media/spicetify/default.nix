519b7eef2a4b7b3b9e133615b73f133fda392c24
Lucas Portela <contato.koish@gmail.com>
2024-04-05 23:32:02 -0300

    spotify: disabled gpu
    dumb, i know
    even discord runs great without that (even though it crashes only in the
    login page)
    but spotify doesn't?

--- home/programs/apps/media/spicetify/default.nix
+++ home/programs/apps/media/spicetify/default.nix
@@ -1,5 +1,6 @@
 {
   inputs,
+  lib,
   pkgs,
   ...
 }: let
@@ -8,9 +9,23 @@
 in {
   imports = [spicetify.homeManagerModule];
 
+  # webcord crashes on the login menu but it works
+  # spotify is lazy i guess
+  xdg.desktopEntries."spotify" = lib.mkForce {
+    name = "Spotify";
+    type = "Application";
+    mimeType = ["x-scheme-handler/spotify"];
+    icon = "spotify-client";
+    exec = "spotify --disable-gpu %U";
+    categories = ["Audio"];
+    terminal = false;
+  };
+
   programs.spicetify = {
     enable = true;
     theme = spice.themes.catppuccin;
+    spotifyPackage = pkgs.spotifywm;
+
     colorScheme = "mocha";
 
     enabledExtensions = with spice.extensions; [
