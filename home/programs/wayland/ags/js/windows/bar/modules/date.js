a79a8da18fa5471901c1533a18533c84777ee2c9
Lucas Portela <contato.koish@gmail.com>
2024-04-07 18:30:12 -0300

    ags: removed GtkCalendar from date
    to be honest, it was quite useless as I already have thunderbird
    calendar.
    i'd like to use gnome-calendar, but the online accounts option is bugged
    to me

--- home/programs/wayland/ags/js/windows/bar/modules/date.js
+++ home/programs/wayland/ags/js/windows/bar/modules/date.js
@@ -1,31 +1,15 @@
-import { App, Utils, Widget } from "../../../imports.js";
+import { Utils, Widget } from "../../../imports.js";
 const { execAsync } = Utils;
-const { Button, Calendar, Label, Window } = Widget;
+const { Button, Label } = Widget;
 
 const TimeLabel = () =>
     Label({ className: "timeLabel" }).poll(1000, (self) =>
         execAsync(["date", "+%d/%m/%y %H:%M"]).then((i) => (self.label = i)),
     );
 
-export const TimeCalendar = () =>
-    Window({
-        name: "TimeCalendar",
-        monitor: 0, // TODO: this is lazy
-        anchor: ["top", "right"],
-        visible: false,
-        child: Calendar({
-            className: "calendar",
-            showDayNames: true,
-            showDetails: true,
-            showHeading: true,
-            showWeekNumbers: true,
-        }),
-    });
-
 export const Date = () =>
     Button({
         className: "clock",
         cursor: "pointer",
         child: TimeLabel(),
-        onClicked: () => App.toggleWindow("TimeCalendar"),
     });
