eb473d466af8b0fa8c53d7447d9a082127b7ed8d
Lucas Portela <contato.koish@gmail.com>
2024-04-06 21:54:36 -0300

    nix: nixified ai binary cache

--- modules/nix/substituters.nix
+++ modules/nix/substituters.nix
@@ -6,6 +6,7 @@
       "https://anyrun.cachix.org"
       "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
+      "https://ai.cachix.org"
       "https://nix-gaming.cachix.org"
     ];
 
@@ -14,6 +15,7 @@
       "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
     ];
   };
