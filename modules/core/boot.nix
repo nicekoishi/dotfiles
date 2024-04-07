161c6dfa95fac0d38f083114395b98a4bf055f13
Lucas Portela <contato.koish@gmail.com>
2024-04-06 21:46:12 -0300

    boot: added kernel parameters
    the main reason plymouth barely shows during boot is not because of
    wrong parameters
    main reason this time is... novideo again

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -8,12 +8,14 @@
     kernelPackages = pkgs.linuxPackages_zen;
 
     initrd = {
+      verbose = false;
       systemd.enable = true;
       supportedFilesystems = ["ntfs"];
     };
 
     loader = {
       efi.canTouchEfiVariables = true;
+      timeout = 0;
       systemd-boot = {
         enable = true;
         configurationLimit = 12;
@@ -23,9 +25,39 @@
 
     consoleLogLevel = 3;
     kernelParams = [
+      # useful stuff
+
+      /*
+         CPU idle behaviour
+      poll: slightly improve performance at cost of a hotter system (not recommended)
+      halt: halt is forced to be used for CPU idle
+      nomwait: Disable mwait for CPU C-states
+      */
+      "idle=nomwait" # poll | halt | nomwait
+
+      # single gpu passthrough is fun
+      "iommu=pt"
+
+      # prevent the kernel from blanking plymouth out of the fb
+      "fbcon=nodefer"
+
+      # disabling cursor in vt
+      "vt.global_cursor_default=0"
+
+      # disable displaying of the builtin linux logo
+      "logo.nologo"
+
+      # silent boot
+      # shush
       "quiet"
-      "systemd-show_status=auto"
+
+      # lowring udev log level
+      "udev.log_level=3"
       "rd.udev.log_level=3"
+
+      # disabling systemd status messages
+      "systemd-show_status=auto"
+      "rd.systemd.show_status=auto"
     ];
 
     plymouth = {
