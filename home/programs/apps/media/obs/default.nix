57730c6abddd9f7e22c664f4907526c923ef37e5
Lucas Portela <contato.koish@gmail.com>
2024-04-10 22:41:41 -0300

    obs: added plugins

--- home/programs/apps/media/obs/default.nix
+++ home/programs/apps/media/obs/default.nix
@@ -8,7 +8,10 @@
 in {
   programs.obs-studio = {
     enable = true;
-    plugins = [];
+    plugins = with pkgs.obs-studio-plugins; [
+      obs-gstreamer
+      obs-vkcapture
+    ];
   };
 
   xdg.configFile."obs-studio/themes" = {
