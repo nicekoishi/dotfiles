2cad65658f188928f0958719fab64a151207bc08
Lucas Portela <contato.koish@gmail.com>
2024-04-11 15:36:34 -0300

    home-manager: adding again options i forgot to
    bruh

--- modules/programs/home-manager.nix
+++ modules/programs/home-manager.nix
@@ -2,7 +2,17 @@
   imports = [inputs.home-manager.nixosModules.default];
 
   home-manager = {
+    # keep yapping
+    verbose = true;
+
+    # parity between nixos' pkgs and hm's packages
     useGlobalPkgs = true;
+
+    # users.users.<name>.packages, do I even use this?
     useUserPackages = true;
+
+    # i actually added this before, but did git reset --hard because of that last commit
+    # actually forgot to add again
+    backupFileExtension = "hm.old";
   };
 }
