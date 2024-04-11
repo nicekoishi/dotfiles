84e0b627abac320917a04273ce5985f640f4ab7f
Lucas Portela <contato.koish@gmail.com>
2024-04-11 13:45:08 -0300

    beets: init

--- home/programs/apps/media/default.nix
+++ home/programs/apps/media/default.nix
@@ -1,7 +1,9 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
+    ffmpeg-full
     playerctl
     viewnior
+    yt-dlp
   ];
 
   programs.mpv = {
--- home/programs/apps/media/music/cantata/default.nix
+++ home/programs/apps/media/music/cantata/default.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   imports = [
-    ./mpd.nix
+    ./beets.nix # get audio track metadata, overall nice utility to have
+    ./mpd.nix # this wouldn't work without mpd, right?
   ];
 
   home.packages = [pkgs.cantata];
