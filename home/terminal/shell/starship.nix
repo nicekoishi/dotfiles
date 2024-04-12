1f42f6f77ab8a7616be3e3026e04d5c51977a8e0
Lucas Portela <contato.koish@gmail.com>
2024-04-12 11:01:26 -0300

    starship: messing around

--- home/terminal/shell/starship.nix
+++ home/terminal/shell/starship.nix
@@ -7,17 +7,32 @@
       add_newline = false;
       scan_timeout = 2;
       command_timeout = 2000;
+      line_break.disabled = false;
 
       character = {
-        success_symbol = "";
-        error_symbol = "";
+        success_symbol = "[](bold green)";
+        error_symbol = "[](bold red)";
       };
 
+      git_commit.commit_hash_length = 7;
+      git_branch.style = "bold purple";
       git_status = {
-        deleted = "✗";
-        modified = "✶";
-        staged = "✓";
-        stashed = "≡";
+        ahead = "⇡ ";
+        behind = "⇣ ";
+        conflicted = " ";
+        deleted = "✗ ";
+        diverged = "⇆ ";
+        modified = "✶ ";
+        renamed = "»";
+        staged = "✓ ";
+        stashed = "≡ ";
+        style = "red";
+        untracked = "?";
+      };
+
+      nix_shell = {
+        symbol = "   ";
+        heuristic = true;
       };
     };
   };
