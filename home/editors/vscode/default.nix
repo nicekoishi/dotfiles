4300d27b2394269e25d07853218aa442dc7c895c
Lucas Portela <contato.koish@gmail.com>
2024-04-16 23:15:59 -0300

    vscode: added extensions

--- home/editors/vscode/default.nix
+++ home/editors/vscode/default.nix
@@ -2,29 +2,36 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
-    mutableExtensionsDir = false;
+    enableExtensionUpdateCheck = true;
+    enableUpdateCheck = true;
 
     # TODO: i may have forgot to add some extensions
-    extensions = with pkgs.vscode-extensions; [
-      # stuff that should be default
-      christian-kohler.path-intellisense
-      usernamehw.errorlens
-      gruntfuggly.todo-tree
+    extensions = with pkgs.vscode-extensions;
+      [
+        # stuff that should be default
+        christian-kohler.path-intellisense
+        formulahendry.code-runner
+        gruntfuggly.todo-tree
+        ibm.output-colorizer
+        irongeek.vscode-env
+        naumovs.color-highlight
+        oderwat.indent-rainbow
+        usernamehw.errorlens
+        yzhang.markdown-all-in-one
 
-      # nix
-      mkhl.direnv
-      jnoortheen.nix-ide
-      kamadorueda.alejandra
+        # cpp
+        ms-vscode.cpptools
+        xaver.clang-format
 
-      # vim keybinds
-      vscodevim.vim
+        # catppuccin theme
+        catppuccin.catppuccin-vsc
+        catppuccin.catppuccin-vsc-icons
 
-      # css colors
-      kamikillerto.vscode-colorize
-
-      # catppuccin theme
-      catppuccin.catppuccin-vsc
-      catppuccin.catppuccin-vsc-icons
-    ];
+        # nix
+        mkhl.direnv
+        bbenoist.nix
+        kamadorueda.alejandra
+      ]
+      ++ [pkgs.vscode-extensions."2gua".rainbow-brackets];
   };
 }
