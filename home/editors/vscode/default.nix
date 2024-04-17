bc1ac5e8beddc5346baa4a4225b297dcb4d04a3d
Lucas Portela <contato.koish@gmail.com>
2024-04-17 18:13:50 -0300

    vscode: added cpp default formatter

--- home/editors/vscode/default.nix
+++ home/editors/vscode/default.nix
@@ -29,9 +29,63 @@
 
         # nix
         mkhl.direnv
-        bbenoist.nix
+        jnoortheen.nix-ide
         kamadorueda.alejandra
       ]
       ++ [pkgs.vscode-extensions."2gua".rainbow-brackets];
+
+    userSettings = {
+      "catppuccin.accentColor" = "teal";
+      "editor.cursorBlinking" = "smooth";
+      "editor.cursorSmoothCaretAnimation" = "explicit";
+      "editor.fontFamily" = "Iosevka Nerd Font Propo";
+      "editor.fontLigatures" = true;
+      "editor.fontSize" = 16;
+      "editor.formatOnSave" = true;
+      "editor.guides.bracketPairs" = true;
+      "editor.minimap.enabled" = false;
+      "editor.smoothScrolling" = true;
+      "nix.enableLanguageServer" = true;
+      "security.workspace.trust.enabled" = false;
+      "terminal.integrated.defaultProfile.linux" = "zsh";
+      "terminal.integrated.gpuAcceleration" = "on";
+      "window.menuBarVisibility" = "toggle";
+      "window.restoreWindows" = "all";
+      "workbench.colorTheme" = "Catppuccin Mocha";
+      "workbench.iconTheme" = "catppuccin-mocha";
+      "workbench.list.smoothScrolling" = true;
+      "workbench.sideBar.location" = "right";
+      "workbench.startupEditor" = "none";
+      "todo-tree.highlights.customHighlight" = {
+        "BUG" = {
+          "icon" = "bug";
+          "background" = "#f38ba8";
+          "type" = "text";
+        };
+        "HACK" = {
+          "icon" = "tools";
+          "background" = "#f9e2af";
+          "type" = "text";
+        };
+        "FIXME" = {
+          "icon" = "flame";
+          "background" = "#f38ba8";
+          "type" = "text";
+        };
+        "XXX" = {
+          "icon" = "x";
+          "background" = "#f9e2af";
+          "type" = "text";
+        };
+        "TODO" = {
+          "icon" = "x";
+          "background" = "#94e2d5";
+          "type" = "text";
+        };
+      };
+      "[cpp]" = {
+        "editor.defaultFormatter" = "xaver.clang-format";
+      };
+    };
   };
 }
