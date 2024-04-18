140c49cc578dbc60911f1626f791ee6b9165717c
Lucas Portela <contato.koish@gmail.com>
2024-04-18 10:47:57 -0300

    flake: update
    following nh merge in nixpkgs, removed input and started using the
    module instead.

--- flake.nix
+++ flake.nix
@@ -68,11 +68,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nh = {
-      url = "github:viperML/nh";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     nix-colors.url = "github:misterio77/nix-colors";
 
     nix-index-db = {
