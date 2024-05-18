c469e597c17cdbf778a71634682b278740148713
Lucas Portela <contato.koish@gmail.com>
2024-05-18 15:17:39 -0300

    chore: flake update

--- modules/core/nvidia.nix
+++ modules/core/nvidia.nix
@@ -3,7 +3,17 @@
   pkgs,
   lib,
   ...
-}: {
+}: let
+  inherit (lib.strings) versionOlder;
+  nvBeta = config.boot.kernelPackages.nvidiaPackages.beta;
+  nvStable = config.boot.kernelPackages.nvidiaPackages.stable;
+
+  # FIXME: 555.42.02 is not on nixos-unstable 24/05/2024
+  nvPackage =
+    if (versionOlder nvBeta.version nvStable.version)
+    then nvStable
+    else nvBeta;
+in {
   boot.blacklistedKernelModules = ["i2c_nvidia_gpu" "nouveau"];
 
   environment = {
@@ -28,24 +38,7 @@
   };
 
   hardware.nvidia = {
-    package = let
-      rcu_patch = pkgs.fetchpatch {
-        url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
-        hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
-      };
-    in
-      config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        version = "535.154.05";
-        sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
-        sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
-        openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
-        settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
-        persistencedSha256 = "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
-
-        patches = [rcu_patch];
-      };
-    # config.boot.kernelPackages.nvidiaPackages.stable;
-    # only bad thing is that aagl doesn't work. but it is stable anywhere else, so let's use this one
+    package = nvPackage;
 
     modesetting.enable = true;
 
@@ -56,12 +49,11 @@
 
     nvidiaPersistenced = true;
 
-    # how funny, it still doesn't work with suspend for me
-    # skill issue?
-    open = lib.mkDefault false;
+    # 555 fixed most of the bugs with suspend, but they still happen sometimes
+    open = true;
 
-    # please work
-    forceFullCompositionPipeline = true;
+    # not need anymore
+    forceFullCompositionPipeline = false;
 
     # useless
     nvidiaSettings = false;
