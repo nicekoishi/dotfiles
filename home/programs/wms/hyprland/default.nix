d4c3822e7ba460a0c53be4e7655252a5e24c45d1
Lucas Portela <contato.koish@gmail.com>
2024-06-16 17:33:42 -0300

    hyprland: key press enables dpms
    i'm that lazy

--- home/programs/wms/hyprland/default.nix
+++ home/programs/wms/hyprland/default.nix
@@ -40,6 +40,7 @@
         disable_hyprland_logo = true;
         disable_splash_rendering = true;
         mouse_move_enables_dpms = true;
+        key_press_enables_dpms = true;
         enable_swallow = true;
         swallow_regex = "^(kitty|footclient|Alacritty)";
       };
@@ -53,7 +54,7 @@
 
         # BUG: disabled tearing as my nvidia gpu doesn't go well with it
         # well, it's not like I have a >60Hz monitor anyway
-        #allow_tearing = true;
+        # allow_tearing = true;
         no_border_on_floating = false;
         apply_sens_to_raw = 0;
       };
