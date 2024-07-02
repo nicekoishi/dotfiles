5783a5c1e80f8fa9868942fb3e6473b1eca93858
Lucas Portela <contato.koish@gmail.com>
2024-07-02 09:42:59 -0300

    quickshell: tidying up
    going to switch to it later, just setting up the service

--- home/services/wayland/quickshell/default.nix
+++ home/services/wayland/quickshell/default.nix
@@ -1,9 +1,44 @@
 {
+  config,
   inputs,
+  lib,
   pkgs,
   ...
 }: let
-  inherit (inputs) quickshell;
+  inherit (lib.modules) mkIf;
+  inherit (lib.meta) getExe;
+
+  quickshell = inputs.quickshell.packages."${pkgs.system}".quickshell;
+
+  requiredDeps = with pkgs; [
+    inputs.hyprland.packages."${pkgs.system}".default
+
+    bash
+  ];
+
+  guiDeps = with pkgs; [
+    mission-center
+  ];
+
+  dependencies = requiredDeps ++ guiDeps;
 in {
-  home.packages = [quickshell.packages."${pkgs.system}".quickshell];
+  systemd.user.services.quickshell = mkIf (!config.programs.ags.enable) {
+    Unit = {
+      Description = "Simple and flexbile QtQuick based desktop shell toolkit.";
+      PartOf = [
+        "tray.target"
+        "graphical-session.target"
+      ];
+    };
+
+    Service = {
+      Environment = "PATH=/run/wrappers/bin:${lib.makeBinPath dependencies}";
+      ExecStart = "${getExe quickshell}";
+
+      Restart = "on-failure";
+      KillMode = "mixed";
+    };
+
+    Install.WantedBy = ["graphical-session.target"];
+  };
 }
