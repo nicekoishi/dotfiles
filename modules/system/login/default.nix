dc2f04a2b4c4257a8b6cbad7eacb4aafb9311433
Lucas Portela <contato.koish@gmail.com>
2024-07-18 17:32:44 -0300

    system/login: fix typo
    why the fuck was an s in there?

--- modules/system/login/default.nix
+++ modules/system/login/default.nix
@@ -11,7 +11,7 @@
   # https://github.com/apognu/tuigreet/issues/68#issuecomment-1586359960
   systemd.services.greetd.serviceConfig = {
     Type = "idle";
-    StandardInputs = "tty";
+    StandardInput = "tty";
     StandardOutput = "tty";
     StandardError = "journal";
     TTYReset = true;
