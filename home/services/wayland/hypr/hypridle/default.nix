92ff9471b1dbb02a7283be0ad73bb3057cef082b
Lucas Portela <contato.koish@gmail.com>
2024-07-21 17:09:09 -0300

    services/wayland: fix hypridle config (again)

--- home/services/wayland/hypr/hypridle/default.nix
+++ home/services/wayland/hypr/hypridle/default.nix
@@ -3,20 +3,25 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  inherit (lib.meta) getExe;
+  hyprctl = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl";
+in {
   services.hypridle = {
     enable = true;
 
     settings = {
       general = {
+        after_sleep_cmd = "${hyprctl} dispatch dpms on";
         before_sleep_cmd = "${pkgs.systemd}/bin/loginctl lock-session";
-        lock_cmd = lib.getExe config.programs.hyprlock.package;
+        lock_cmd = "${pkgs.procps}/bin/pidof hyprlock || ${getExe config.programs.hyprlock.package}";
       };
 
       listener = [
         {
           timeout = 300;
-          on-timeout = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl dispatch dpms off";
+          on-timeout = "${hyprctl} dispatch dpms off";
+          on-resume = "${hyprctl} dispatch dpms on";
         }
         {
           timeout = 1800;
