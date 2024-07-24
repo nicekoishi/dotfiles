b10cb8ae1a142f2a8924867888f6da78f929b579
Lucas Portela <contato.koish@gmail.com>
2024-07-24 11:08:52 -0300

    utilities/oom: protect more priority services
    xwayland crashing was funny.
    also, don't rebase like that ever again

--- modules/system/utilities/oom/default.nix
+++ modules/system/utilities/oom/default.nix
@@ -1,4 +1,10 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib.strings) concatStringsSep;
+in {
   # begone the times I locked out my system when running sketchy stuff and had to reboot
   # i really didn't knew about this, thx raf
   services.earlyoom = {
@@ -6,14 +12,30 @@
     enableNotifications = true;
     freeMemThreshold = 2;
     freeSwapThreshold = 2;
-    extraArgs = [
+    extraArgs = let
+      avoid = concatStringsSep "|" [
+        "Hyprland"
+        "Xwayland"
+        "dbus-.*"
+        "gpg-agent"
+        "ssh-agent"
+        "systemd"
+        "systemd-.*"
+      ];
+      prefer = concatStringsSep "|" [
+        ".*.exe"
+        "Isolated Web Co"
+        "Web Content"
+        "chromium.*"
+        "electron" # imagine screensharing making the pc laggy for some reason
+        "java."
+      ];
+    in [
       "-g" # kill all processes within a process group
-      "--avoid '^(Hyprland|chromium-browser)$'" # it would be funny if it did so
-      "--prefer '^(electron|.*.exe)$'" # that guy really hates electron huh, but I kinda do too
+      "--avoid '^(${avoid})$'" # it would be funny if it did so
+      "--prefer '^(${prefer})$'" # that guy really hates electron huh, but I kinda do too
     ];
 
-    # it should write the logs to the journal
-    # but i am stupid so it may not work?
     killHook = pkgs.writeShellScript "earlyoom-kill-hook" ''
       echo "Process $EARLYOOM_NAME ($EARLYOOM_PID) was killed" | ${pkgs.systemd}/bin/systemd-cat -t "EarlyOOM" -p 3
     '';
