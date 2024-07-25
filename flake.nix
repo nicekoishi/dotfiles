ac5177a29bbcb8482f55d9aa09a07764b43c99f2
Lucas Portela <contato.koish@gmail.com>
2024-07-25 11:31:06 -0300

    flake: cleanup

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux"];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
 
       imports = [
         ./hosts
--- flake/pkgs/default.nix
+++ flake/pkgs/default.nix
@@ -1,20 +1,18 @@
 {
-  systems = [
-    "x86_64-linux"
-    "aarch64-linux"
-  ];
-
   perSystem = {
     config,
     pkgs,
     ...
   }: {
-    packages = {
-      funny-plymouth = pkgs.callPackage ./funny-plymouth {};
+    packages = let
+      inherit (pkgs) callPackage;
+    in {
+      funny-plymouth = callPackage ./funny-plymouth {};
 
-      suncord = pkgs.callPackage ./suncord {};
-      suncord-web-extension = pkgs.callPackage ./suncord {buildWebExtension = true;};
-      webcord-suncord = pkgs.callPackage ./webcord-suncord {
+      suncord = callPackage ./suncord {};
+      suncord-web-extension = callPackage ./suncord {buildWebExtension = true;};
+
+      webcord-suncord = callPackage ./webcord-suncord {
         inherit (config.packages) suncord-web-extension;
       };
     };
