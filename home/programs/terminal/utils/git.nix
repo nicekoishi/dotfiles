5d7cae6429a9e2a2214e7b45e5cc415dc5c57c52
Lucas Portela <contato.koish@gmail.com>
2024-07-28 19:58:27 -0300

    utils/git: add gpg key
    am i stupid?

--- home/programs/terminal/utils/git.nix
+++ home/programs/terminal/utils/git.nix
@@ -5,6 +5,10 @@
     enable = true;
 
     ignores = ["*~" "node_modules" "target"];
+    signing = {
+      signByDefault = true;
+      key = "9F726CE991F7ACD8";
+    };
 
     userName = "Lucas Portela";
     userEmail = "contato.koish@gmail.com";
