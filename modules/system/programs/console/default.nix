4a4831f4675e147a00b00a6e6553ea1c6745d6d1
Lucas Portela <contato.koish@gmail.com>
2024-08-02 19:45:11 -0300

    programs/console: add font

--- modules/system/programs/console/default.nix
+++ modules/system/programs/console/default.nix
@@ -1,5 +1,12 @@
-{
+{pkgs, ...}: {
   imports = [
     ./zsh.nix
   ];
+
+  console = {
+    earlySetup = true;
+    keyMap = "br-abnt2";
+    font = "${pkgs.terminus_font}/share/consolefonts/ter-v24b.psf.gz";
+    packages = [pkgs.terminus_font];
+  };
 }
