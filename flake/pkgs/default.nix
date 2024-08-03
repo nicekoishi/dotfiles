cc144de31c7fe5230b380cbc7fcbaa7aee8796a1
Lucas Portela <contato.koish@gmail.com>
2024-08-02 21:36:12 -0300

    flake/pkgs: deprecate suncord
    it was a fun attempt though

--- flake/pkgs/default.nix
+++ flake/pkgs/default.nix
@@ -1,20 +1,9 @@
 {
-  perSystem = {
-    config,
-    pkgs,
-    ...
-  }: {
+  perSystem = {pkgs, ...}: {
     packages = let
       inherit (pkgs) callPackage;
     in {
       funny-plymouth = callPackage ./funny-plymouth {};
-
-      suncord = callPackage ./suncord {};
-      suncord-web-extension = callPackage ./suncord {buildWebExtension = true;};
-
-      webcord-suncord = callPackage ./webcord-suncord {
-        inherit (config.packages) suncord-web-extension;
-      };
     };
   };
 }
