3fd98993f2622c4f66022a845afb6145acbab9db
Lucas Portela <contato.koish@gmail.com>
2024-08-06 19:56:47 -0300

    video/nvidia: lock driver version
    damn this thing is annoying

--- modules/roles/desktop/hardware/video/nvidia/default.nix
+++ modules/roles/desktop/hardware/video/nvidia/default.nix
@@ -6,17 +6,17 @@
 }: let
   inherit (builtins) elem;
   inherit (lib.modules) mkIf;
-  inherit (lib.strings) versionOlder;
 
   cfg = config.nice.host;
 
-  nvBeta = config.boot.kernelPackages.nvidiaPackages.beta;
-  nvStable = config.boot.kernelPackages.nvidiaPackages.stable;
-
-  nvPackage =
-    if (versionOlder nvBeta.version nvStable.version)
-    then nvStable
-    else nvBeta;
+  nvPackage = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "555.52.04";
+    sha256_64bit = "sha256-nVOubb7zKulXhux9AruUTVBQwccFFuYGWrU1ZiakRAI=";
+    sha256_aarch64 = "sha256-Kt60kTTO3mli66De2d1CAoE3wr0yUbBe7eqCIrYHcWk=";
+    openSha256 = "sha256-wDimW8/rJlmwr1zQz8+b1uvxxxbOf3Bpk060lfLKuy0=";
+    settingsSha256 = "sha256-PMh5efbSEq7iqEMBr2+VGQYkBG73TGUh6FuDHZhmwHk=";
+    persistencedSha256 = "sha256-KAYIvPjUVilQQcD04h163MHmKcQrn2a8oaXujL2Bxro=";
+  };
 in {
   config = mkIf (elem "nvidia" cfg.gpu) {
     boot = {
