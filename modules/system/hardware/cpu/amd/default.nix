84731b8623faf7401c4ea381eb3a3ca17f1872bb
Lucas Portela <contato.koish@gmail.com>
2024-08-26 22:34:15 -0300

    cpu/amd: fixing module
    Now straight from nixos-hardware.
    Why was I even loading amd-pstate when it's builtin after kernel 6.1?

--- modules/system/hardware/cpu/amd/default.nix
+++ modules/system/hardware/cpu/amd/default.nix
@@ -5,7 +5,7 @@
   ...
 }: let
   inherit (lib.modules) mkIf mkMerge;
-  inherit (lib.strings) versionAtLeast;
+  inherit (lib.strings) versionAtLeast versionOlder;
 
   cfg = config.nice.host;
   kver = config.boot.kernelPackages.kernel.version;
@@ -19,13 +19,23 @@ in {
       {
         kernelModules = [
           "kvm-amd"
-          "amd-pstate"
           "zenpower"
           "msr"
         ];
+        blacklistedKernelModules = ["k10temp"];
         kernelParams = ["iommu=pt"];
         extraModulePackages = [config.boot.kernelPackages.zenpower];
       }
+
+      # https://github.com/NixOS/nixos-hardware/blob/9fc19be21f0807d6be092d70bf0b1de0c00ac895/common/cpu/amd/pstate.nix
+      (mkIf ((versionAtLeast kver "5.17") && (versionOlder kver "6.1")) {
+        kernelParams = ["initcall_blacklist=acpi_cpufreq_init"];
+        kernelModules = ["amd-pstate"];
+      })
+
+      (mkIf ((versionAtLeast kver "6.1") && (versionAtLeast kver "6.3")) {
+        kernelParams = ["amd_pstate=passive"];
+      })
 
       (mkIf (versionAtLeast kver "6.3") {
         kernelParams = ["amd-pstate=active"];
