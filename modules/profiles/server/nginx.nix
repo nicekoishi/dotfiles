4a3afff223417e1575491f34356c1481a4456044
Lucas Portela <contato.koish@gmail.com>
2024-09-24 20:07:43 -0300

    server/nginx: censor ips and sensitive query parameters
    This is quite the nuclear solution, as $request_uri is entirely censored
    instead of only the relevant bits...
    Any NGINX wizards there?

--- modules/profiles/server/nginx.nix
+++ modules/profiles/server/nginx.nix
@@ -56,6 +56,34 @@ in {
           # https://github.com/NotAShelf/nyx/blob/440441c19369763fbd512e71bb61807fce27e3c0/modules/core/roles/server/system/services/nginx.nix#L68
           add_header Strict-Transport-Security $hsts_header;
 
+          # Couldn't find the exact source, but here is the oldest I found:
+          # https://dawnbringer.net/blog/901/nginx_ip_masking_anonymize_ip-logging
+          # This defines a new map that anonymize $remote_addr, replacing the last
+          # octet of IPv4 addresses and taking leaving only two segments of IPv6 addresses
+          # 192.168.1.10 -> 192.168.1.0
+          # 2001:0db8:... -> 2001:0db8::
+          map $remote_addr $remote_addr_anon {
+            ~(?P<ip>\d+\.\d+\.\d+)\.    $ip.0;
+            ~(?P<ip>[^:]+:[^:]+):       $ip::;
+            default                     0.0.0.0;
+          }
+
+          # Define a new map that censors url query strings that have paramaters with sensitive info
+          # This will censor the entire url, so it should only be used for logging purposes!
+          # We could set multiple if's here, though...
+          map $request_uri $request_uri_anon {
+            ~[?&](t|s|p|jwt|api_key)=         "[REDACTED]";
+            default                           $request_uri;
+          }
+
+          # Define a log format anon that closely follows combined, but instead anonymizing $remote_addr.
+          # This also attempts to anonymize any sensitive information sent using GET, like Navidrome.
+          log_format anon '$remote_addr_anon - $remote_user [$time_local] '
+                          '"$request_method $request_uri_anon $server_protocol" $status $body_bytes_sent '
+                          '"$http_referer" "$http_user_agent"';
+
+          access_log /var/log/nginx/access.log anon;
+
           proxy_cookie_path / "/; secure; HttpOnly; SameSite=strict";
         '';
       };
