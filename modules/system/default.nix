7b08015fb3d50398164218bc5f9da7273b46e57f
Lucas Portela <contato.koish@gmail.com>
2024-09-25 19:23:35 -0300

    modules/system: reduce directory depth

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -1,9 +1,15 @@
-{
+{lib, ...}: let
+  inherit (lib.modules) mkDefault;
+in {
   imports = [
-    ./core
+    ./boot
+    ./environment
     ./hardware
     ./networking
     ./programs
+    ./security
     ./services
   ];
+
+  system.stateVersion = mkDefault "23.11";
 }
