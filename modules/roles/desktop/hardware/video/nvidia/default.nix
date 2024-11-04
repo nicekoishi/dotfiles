4d915c274b57e28b69c942dd8130fe5c523e24ed
Lucas Portela <contato.koish@gmail.com>
2024-11-04 14:56:14 -0300

    hardware/nvidia: driver update to 565.57.01

--- modules/roles/desktop/hardware/video/nvidia/default.nix
+++ modules/roles/desktop/hardware/video/nvidia/default.nix
@@ -11,12 +11,12 @@
   dev = cfg.host;
 
   nvPackage = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    version = "560.35.03";
-    sha256_64bit = "sha256-8pMskvrdQ8WyNBvkU/xPc/CtcYXCa7ekP73oGuKfH+M=";
-    sha256_aarch64 = "sha256-s8ZAVKvRNXpjxRYqM3E5oss5FdqW+tv1qQC2pDjfG+s=";
-    openSha256 = "sha256-/32Zf0dKrofTmPZ3Ratw4vDM7B+OgpC4p7s+RHUjCrg=";
-    settingsSha256 = "sha256-kQsvDgnxis9ANFmwIwB7HX5MkIAcpEEAHc8IBOLdXvk=";
-    persistencedSha256 = "sha256-E2J2wYYyRu7Kc3MMZz/8ZIemcZg68rkzvqEwFAL3fFs=";
+    version = "565.57.01";
+    openSha256 = "sha256-/tM3n9huz1MTE6KKtTCBglBMBGGL/GOHi5ZSUag4zXA=";
+    persistencedSha256 = "sha256-hdszsACWNqkCh8G4VBNitDT85gk9gJe1BlQ8LdrYIkg=";
+    settingsSha256 = "sha256-H7uEe34LdmUFcMcS6bz7sbpYhg9zPCb/5AmZZFTx1QA=";
+    sha256_64bit = "sha256-buvpTlheOF6IBPWnQVLfQUiHv4GcwhvZW3Ks0PsYLHo=";
+    sha256_aarch64 = "sha256-aDVc3sNTG4O3y+vKW87mw+i9AqXCY29GVqEIUlsvYfE=";
   };
 in {
   config = mkIf (elem "nvidia" dev.gpu) {
