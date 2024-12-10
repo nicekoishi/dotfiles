90e923fc6e1017383c546c8972ef60c3888090ba
Lucas Portela <contato.koish@gmail.com>
2024-12-10 12:49:36 -0300

    video/nvidia: update driver to stable release
    And with it, we don't need to use kernel 6.11 anymore as the patch was
    already merged.

--- modules/roles/desktop/hardware/video/nvidia/default.nix
+++ modules/roles/desktop/hardware/video/nvidia/default.nix
@@ -10,18 +10,26 @@
   cfg = config.nice;
   dev = cfg.host;
 
+  # NOTE: This is here because I can't be bothered to check if the driver works
+  # after every update, so we update the rest of the system first and then this driver.
   nvPackage = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    version = "565.57.01";
-    openSha256 = "sha256-/tM3n9huz1MTE6KKtTCBglBMBGGL/GOHi5ZSUag4zXA=";
-    persistencedSha256 = "sha256-hdszsACWNqkCh8G4VBNitDT85gk9gJe1BlQ8LdrYIkg=";
-    settingsSha256 = "sha256-H7uEe34LdmUFcMcS6bz7sbpYhg9zPCb/5AmZZFTx1QA=";
-    sha256_64bit = "sha256-buvpTlheOF6IBPWnQVLfQUiHv4GcwhvZW3Ks0PsYLHo=";
-    sha256_aarch64 = "sha256-aDVc3sNTG4O3y+vKW87mw+i9AqXCY29GVqEIUlsvYfE=";
+    version = "565.77";
+    sha256_64bit = "sha256-CnqnQsRrzzTXZpgkAtF7PbH9s7wbiTRNcM0SPByzFHw=";
+    sha256_aarch64 = "sha256-LSAYUnhfnK3rcuPe1dixOwAujSof19kNOfdRHE7bToE=";
+    openSha256 = "sha256-Fxo0t61KQDs71YA8u7arY+503wkAc1foaa51vi2Pl5I=";
+    settingsSha256 = "sha256-VUetj3LlOSz/LB+DDfMCN34uA4bNTTpjDrb6C6Iwukk=";
+    persistencedSha256 = "sha256-wnDjC099D8d9NJSp9D0CbsL+vfHXyJFYYgU3CwcqKww=";
   };
 in {
   config = mkIf (elem "nvidia" dev.gpu) {
     boot = {
-      blacklistedKernelModules = ["i2c_nvidia_gpu" "nouveau"];
+      blacklistedKernelModules = [
+        # USB-C driver, my 1660 doesn't need it.
+        "i2c_nvidia_gpu"
+
+        "nouveau"
+      ];
+
       kernelParams = [
         "nvidia_drm.fbdev=1"
         "nvidia.NVreg_EnableGpuFirmware=0"
@@ -66,7 +74,6 @@ in {
         open = lib.mkForce false;
 
         # weird fullscreen bug when trying to use fsr, so lets disable it
-        # it didn't help, as it was a hyprland problem. one day it will be fixed
         forceFullCompositionPipeline = lib.mkForce false;
 
         # useless
