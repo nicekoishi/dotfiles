91f3992036fae8cef4904c12d30a7ea526c94ccd
Lucas Portela <contato.koish@gmail.com>
2025-01-21 22:26:20 -0300

    media/music: temporarily disable feishin
    Currently using an EOL version of electron.

--- home/programs/apps/media/music/default.nix
+++ home/programs/apps/media/music/default.nix
@@ -7,5 +7,8 @@
     ./beets.nix # music tagging app, nice utility to be paired with kid3
   ];
 
-  home.packages = with pkgs; [feishin];
+  home.packages = [
+    # As of 21/01/2024, this package uses electron 31.7.7 which is EOL
+    # pkgs.feishin
+  ];
 }
